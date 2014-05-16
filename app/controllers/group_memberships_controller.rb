22014012707a009a76c612b123d8a8376a1b6bc2
decatan <ys@thecompany.sg>
2014-05-16 20:26:09 +0800

    halfway done with the groups controller and view

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -2,8 +2,20 @@ class GroupMembershipsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
+		@user = User.find(params[:group_membership][:user_id])
+		current_user.join!(@group)
+		respond_to do |format|
+			format.html { redirect_to @group }
+			format.js
+		end
 	end
 
 	def destroy
+		@user = Relationship.find(params[:id]).followed
+		current_user.unfollow!(@user)
+		respond_to do |format|
+			format.html { redirect_to @user }
+			format.js
+		end
 	end
 end
